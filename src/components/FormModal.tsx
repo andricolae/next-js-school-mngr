be174f2b52008fbfacdec03304094c8b7a93d6b7
Elisei-26 <pralaelisei76@gmail.com>
2025-08-06 14:04:51 +0300

    Fix bug delete lessons not working

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -114,11 +114,15 @@ const FormModal = ({ table, type, data, id, relatedData }: FormContainerProps & 
                 setOpen(false);
                 router.refresh();
             }
+            if (state.error) {
+                toast(`${table} nu a putut fi sters. Exista o tema sau un examen asociat`);
+                setOpen(false);
+            }
         }, [state, router]);
 
         return type === "delete" && id ? (
             <form action={formAction} className="p-4 flex flex-col gap-4">
-                <input type="text|number" name="id" value={id} hidden />
+                <input type="text|number" name="id" value={id} hidden readOnly/>
                 <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
                 <button className="bg-red-600 text-white py-3 px-4 rounded-md border-none w-max self-center">Delete</button>
             </form>
