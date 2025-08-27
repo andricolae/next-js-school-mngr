d38d78f6ad04380e16c5401fc87769a406897137
Andrei Nicolae <andreicalutiu@gmail.com>
2025-08-27 15:45:59 +0300

    added the modules and holidays in lib as a central management file

---
+++ src/lib/holidays.ts
@@ -0,0 +1,58 @@
+export type HolidayType = {
+    id: number;
+    name: string;
+    date: string;
+};
+
+export const nationalHolidays: HolidayType[] = [
+    {
+        id: 1,
+        name: "Ziua națională",
+        date: "2025-12-01"
+    },
+    {
+        id: 2,
+        name: "Botezul Domnului",
+        date: "2026-01-06"
+    },
+    {
+        id: 3,
+        name: "Sf. Ioan",
+        date: "2026-01-07"
+    },
+    {
+        id: 4,
+        name: "Ziua muncii",
+        date: "2026-05-01"
+    },
+    {
+        id: 5,
+        name: "Ziua copilului",
+        date: "2026-06-01"
+    },
+    {
+        id: 6,
+        name: "Vacanță februarie - luni",
+        date: "2026-02-23"
+    },
+    {
+        id: 7,
+        name: "Vacanță februarie - marți",
+        date: "2026-02-24"
+    },
+    {
+        id: 8,
+        name: "Vacanță februarie - miercuri",
+        date: "2026-02-25"
+    },
+    {
+        id: 9,
+        name: "Vacanță februarie - joi",
+        date: "2026-02-26"
+    },
+    {
+        id: 10,
+        name: "Vacanță februarie - vineri",
+        date: "2026-02-27"
+    },
+];
